b0d30b966952cb9e0644563129dae2b4bcee721c
Tchuente Pierre <pierre.armand@takealot.com>
2023-01-09 08:57:16 +0200

    Adding stattus route registration

--- backend_api/service_api/server.py
+++ backend_api/service_api/server.py
@@ -2,6 +2,7 @@ from pyramid.config import Configurator
 
 from service_api.config import Config
 from service_api.helpers import pyramid_renderers as renderer_factory
+from service_api.routes.register_routes import RegisterRoute
 
 
 def serve(global_config, **settings):
@@ -17,6 +18,7 @@ def serve(global_config, **settings):
         name="json", factory=renderer_factory.create_json_renderer()
     )
     pyramid_config.add_static_view(name="swagger", path="api_docs")
+    RegisterRoute.register_api_routes(pyramid_config)
 
     logger.info("WSGI App being returned...")
     return pyramid_config.make_wsgi_app()
